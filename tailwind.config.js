f08fce7d158fe0008b7683e26950d3357257a7ca
Zack F <zackaryfotheringham@hotmail.com>
2023-01-20 12:44:59 -0500

    add custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./public/index.html",
     "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "can-hover": { raw: "(hover: hover)" }
+      },
+      colors: {
+        'C-off-white': 'rgb(207, 207, 192)',
+        'C-light-gray': 'rgb(192, 192, 186)'
+      }
+  }
   },
   plugins: [],
 }
